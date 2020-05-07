228da29b8f7b60690c461cbedc6fa229a0fc1d86
Dhananjay Goratela <41978707+dhgoratela@users.noreply.github.com>
2020-05-07 16:34:33 +0530

    Removing lines to clone Neural_DRS repo
    Since already cloning the repo at the first step, we don't need this line again

--- src/marian_cpu_preset.sh
+++ src/marian_cpu_preset.sh
@@ -9,5 +9,3 @@ sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
 sudo sh -c 'echo deb https://apt.repos.intel.com/mkl all main > /etc/apt/sources.list.d/intel-mkl.list'
 sudo apt-get update
 yes | sudo apt-get install intel-mkl-64bit-2019.4-070
-git clone https://github.com/RikVN/Neural_DRS.git
-chmod +x ./Neural_DRS/src/setup.sh
